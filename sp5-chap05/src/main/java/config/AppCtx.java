576b823e4505b1e42eadf9ec85b017c9d2659448
notsalhoon <39642196+notsalhoon@users.noreply.github.com>
2021-02-19 23:05:53 +0900

    Add files via upload

---
+++ sp5-chap05/src/main/java/config/AppCtx.java
@@ -0,0 +1,62 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import spring.MemberPrinter;
+import spring.MemberSummaryPrinter;
+import spring.VersionPrinter;
+
+@Configuration
+@ComponentScan(basePackages = {"spring"})
+public class AppCtx {
+	
+	/*
+	@Bean
+	public MemberDao memberDao() {
+		return new MemberDao();
+	}
+	
+	@Bean
+	public MemberRegisterService memberRegSvc() {
+		return new MemberRegisterService(memberDao());
+	}
+	
+	@Bean
+	public ChangePasswordService changePwdSvc() {
+		return new ChangePasswordService(); //@Autowired로 자동 주입
+	}
+	
+	@Bean
+	public MemberListPrinter listPrinter() {
+		return new MemberListPrinter();
+	}
+	
+	@Bean
+	public MemberInfoPrinter infoPrinter() {
+		return new MemberInfoPrinter(); //@Autowired로 자동주입
+	}
+	ComponentScan으로 스캔         */
+	
+	@Bean
+	@Qualifier("printer")
+	public MemberPrinter memberPrinter1() {
+		return new MemberPrinter();
+	}
+	
+	@Bean
+	@Qualifier("summaryPrinter")
+	public MemberSummaryPrinter memberPrinter2() {
+		return new MemberSummaryPrinter();
+	}
+	
+	@Bean
+	public VersionPrinter versionPrinter() {
+		VersionPrinter versionPrinter = new VersionPrinter();
+		versionPrinter.setMajorVersion(5);
+		versionPrinter.setMinorVersion(0);
+		return versionPrinter;
+	}
+}
