e075e5538d726cf30d363cc5efc1d5165f970307
notsalhoon <39642196+notsalhoon@users.noreply.github.com>
2021-02-19 22:59:44 +0900

    Add files via upload

---
+++ sp5-chap02/src/main/java/chap02/Main.java
@@ -0,0 +1,18 @@
+package chap02;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx =
+				new AnnotationConfigApplicationContext(AppContext.class);
+		Greeter g1 = ctx.getBean("greeter", Greeter.class);
+		Greeter g2 = ctx.getBean("greeter", Greeter.class);
+//		String msg = g.greet("스프링");
+		System.out.println("(g1 == g2) = " + (g1 == g2));
+		ctx.close();
+
+	}
+
+}
