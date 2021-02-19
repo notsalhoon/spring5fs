e60e5f59500dc7e8fe06033a13e679bc006e9086
notsalhoon <39642196+notsalhoon@users.noreply.github.com>
2021-02-19 23:10:39 +0900

    Add files via upload

---
+++ sp5-chap13/src/main/java/spring/DuplicateMemberException.java
@@ -0,0 +1,13 @@
+package spring;
+
+public class DuplicateMemberException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DuplicateMemberException(String message) {
+		super(message);
+	}
+}
---
+++ sp5-chap13/src/main/java/spring/WrongIdPasswordException.java
@@ -0,0 +1,10 @@
+package spring;
+
+public class WrongIdPasswordException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
